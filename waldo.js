a04b96f5388364644a2b5505f121f4ae90415ddd
Michael Chung <michael.chung1994@gmail.com>
2018-11-29 20:08:08 +0000

    replaced regular for loop wiht forEach call

--- waldo.js
+++ waldo.js
@@ -1,20 +1,14 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(element, i) {
+    if(element === "Waldo") {
+      found(i);
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
-  console.log("Found him at " + "index " + index + "!");
+  console.log("Found him at index " + index + "!");
 }
-
-// function forEach(array, cb) {
-//   for(var i = 0; i >= array.length; i++) {
-
-//   }
-// }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
