b0f0855f3dbbf0a78c01347d0f648cacdfb8c4e1
Michael Chung <michael.chung1994@gmail.com>
2018-11-29 21:09:18 +0000

    finalized result with mapping

---
+++ mapping/map.js
@@ -0,0 +1,16 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(function(object) {
+  var x = object.x;
+  var y = object.y;
+
+  return Math.sqrt((x * x) + (y * y));
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
